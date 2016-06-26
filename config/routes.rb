7b2a6ae0f327a4869f0416f776ba5ff2244c2e53
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2016-06-26 14:34:17 -0700

    Updated routes to use home as root and fixed paths

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'pages/home', to: 'pages#home'
-  get 'pages/about', to: 'pages#about'
+  root'pages#home'
+  get 'about', to: 'pages#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
