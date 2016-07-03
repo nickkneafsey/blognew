c11aa4bfc0ee5ff0461df1c02100e998fe877803
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2016-07-03 14:52:25 -0700

    Add secure pw to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703203141) do
+ActiveRecord::Schema.define(version: 20160703213646) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160703203141) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
